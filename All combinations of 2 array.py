f6e289aa52f184e3df584aa4953ffeb949bda7eb
AmazonianPanda <howyoudoing1618@gmail.com>
2020-10-22 22:57:52 +0530

    readme final modify

--- All combinations of 2 array.py
+++ All combinations of 2 array.py
@@ -7,9 +7,9 @@ arr5 = "linjo"
 [print(a,b,c,d,e) for a in arr for b in arr2
                      for c in arr3 for d in arr4
                          for e in arr5
-                 if not (a  in (b,c,d,e) or
-                     b  in (a,c,d,e) or
-                     c  in (a,b,d,e) or
-                     d  in (a,b,c,e) or
-                     e  in (a,b,c,d))]
+                            if not (a  in (b,c,d,e) or
+                                b  in (a,c,d,e) or
+                                c  in (a,b,d,e) or
+                                d  in (a,b,c,e) or
+                                e  in (a,b,c,d))]
 
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # MySmallProjects
 ### A list of my small scale projects which are too small to have a repo of their own.
-
+#### dont even bother to look
